682e145a532b93b4fd6a23fc07adacb077a94788
Joachim Wallsin <joachim@wallsin.com>
2015-03-14 12:45:38 +0100

    Grunt

---
+++ themes/newchallenge/Gruntfile.js
@@ -0,0 +1,47 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    compass: {                  // Task
+      dist: {                   // Target
+        options: {              // Target options
+          sassDir: 'scss',
+          cssDir: 'static/css',
+          environment: 'production',
+          //style: 'compressed'
+        }
+      },
+      dev: {                    // Another target
+        options: {
+          sassDir: 'scss',
+          cssDir: 'static/css',
+          //style: 'expanded'
+        }
+      }
+    },
+    concat: {
+      js: {
+        src: [
+          'js/jquery.min.js',
+          'js/jquery.ui.widget.min.js',
+          'js/jquery.doubletaptogo.js',
+          'js/app.js'
+        ],
+        dest: 'static/js/script.js'
+      }
+    },
+    uglify: {
+      build: {
+        src: 'static/js/script.js',
+        dest: 'static/js/script.min.js'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default', ['compass:dist', 'concat:js', 'uglify']);
+  grunt.registerTask('dev', ['compass:dev', 'concat:js']);
+
+};
