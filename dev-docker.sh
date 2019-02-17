2915fb2f1520f29291748d463775d49cde0b93e4
Joachim Wallsin <joachim@wallsin.com>
2019-02-17 15:47:20 +0100

    dev-docker updates

--- dev-docker.sh
+++ dev-docker.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-docker run --rm -it -v $(pwd):/sources:z -p 4000:4000 ruby
+docker run --rm -it -v $(pwd):/sources:z -p 4000:4000 ruby /bin/bash
 
-# gem install bundler
-# bundle install
+# cd /sources
+# gem install bundler && \
+# bundle install && \
 # bundle exec jekyll serve --config _config.yml,_dev.yml --host 0.0.0.0 --port 4000 -w
