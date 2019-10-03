96480fde6f80010a1892dca7a4c7a9fcd6c94768
Joachim Wallsin <joachim.wallsin@fbinhouse.se>
2019-10-03 14:02:09 +0200

    Remove docker instance after running

--- dev-docker.sh
+++ dev-docker.sh
@@ -2,5 +2,9 @@
 
 docker build -t newchallenge .
 
-docker run -it -v $(pwd):/sources:z -p 4000:4000 -w "/sources" newchallenge \
+docker run --rm -it \
+-v $(pwd):/sources:z \
+-p 4000:4000 \
+-w "/sources" \
+newchallenge \
 bundle exec jekyll serve --config _config.yml,_dev.yml --host 0.0.0.0 --port 4000 -w
