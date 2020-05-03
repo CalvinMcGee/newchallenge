d65c371439f3fe2647e83255b281c765ce80f907
Joachim Wallsin <joachim@wallsin.com>
2020-05-03 08:38:13 +0200

    Adding instagram token from new rakefile

--- dev-docker.sh
+++ dev-docker.sh
@@ -6,5 +6,6 @@ docker run --rm -it \
 -v $(pwd):/sources:z \
 -p 4000:4000 \
 -w "/sources" \
+-e INSTAGRAM_CONFIG_URL=https://vziovhnlhi.execute-api.eu-north-1.amazonaws.com/production/instagram \
 newchallenge \
 bundle exec jekyll serve --config _config.yml,_dev.yml --host 0.0.0.0 --port 4000 -w
