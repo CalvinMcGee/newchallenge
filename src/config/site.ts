4bd859e7a694e84347d84f4393578b98f7156d7d
Joachim Wallsin <joachim.wallsin@consid.se>
2026-03-14 16:00:43 +0100

    feat: site wide nav

--- src/config/site.ts
+++ src/config/site.ts
@@ -5,4 +5,42 @@ export const site = {
   phone_short: "+46735833329",
   description: "New Challenge erbjuder idrottspsykologisk rådgivning och föreläsningar.",
   url: "https://newchallenge.se",
+  nav: [
+    {
+      title: "Hem",
+      url: "/"
+    },
+    {
+      title: "Tjänster",
+      url: "/tjanster-pris/",
+      items: [
+        {
+          title: "Idrottspsykologisk föreläsning",
+          url: "/tjanster-pris/idrottspsykologisk-forelasning/"
+        },
+        {
+          title: "Individuell rådgivning",
+          url: "/tjanster-pris/individuell-radgivning/"
+        }
+      ]
+    },
+    {
+      title: "Om företaget",
+      url: "/om-foretaget/",
+      items: [
+        {
+          title: "Nätverk/Samarbeten",
+          url: "/om-foretaget/natverk-samarbeten/"
+        },
+        {
+          title: "Om sidan",
+          url: "/om-sidan/"
+        }
+      ]
+    },
+    {
+      title: "Kontakt",
+      url: "#kontakt"
+    }
+  ],
 };
