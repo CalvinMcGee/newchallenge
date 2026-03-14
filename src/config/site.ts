d44785d0072181098544fbc400c4dd03a66bb605
Joachim Wallsin <joachim.wallsin@consid.se>
2026-03-14 15:31:50 +0100

    feat: correct title tag

---
+++ src/config/site.ts
@@ -0,0 +1,8 @@
+export const site = {
+  title: "New Challenge",
+  email: "marcus@newchallenge.se",
+  phone: "0735-83 33 29",
+  phone_short: "+46735833329",
+  description: "New Challenge erbjuder idrottspsykologisk rådgivning och föreläsningar.",
+  url: "https://newchallenge.se",
+};
