dbbcde3675b377f2dfe47ad23358dcbb7432c8ab
Joachim Wallsin <joachim.wallsin@consid.se>
2026-03-15 16:51:26 +0100

    feat: contact text

--- src/config/site.ts
+++ src/config/site.ts
@@ -43,6 +43,14 @@ export const site = {
       url: "#kontakt"
     }
   ],
+  contact: {
+    title: "Kontakta oss",
+    text: `Kontakta Marcus Nyqvist om du har frågor kring rådgivning eller föreläsningar.
+
+Skicka en intresseanmälan här på hemsidan direkt eller till [marcus@newchallenge.se](mailto:marcus@newchallenge.se).
+
+Eller ring på [0735-83 33 29](tel:+46735833329).`,
+  },
   footer: {
     text: [
       "Idrottpsykologisk rådgivning för individ och grupp.",
